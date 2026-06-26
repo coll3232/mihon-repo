85b4483a14b4e9bd3e26049b9083dc247e25f7dd
coll3232 <coll3232@gmail.com>
2026-06-26 10:37:45 +0900

    temp: route baseUrl to local server to capture user-agent

--- ext/src/main/kotlin/eu/kanade/tachiyomi/extension/ko/newtokitoki25/NewToki.kt
+++ ext/src/main/kotlin/eu/kanade/tachiyomi/extension/ko/newtokitoki25/NewToki.kt
@@ -50,7 +50,7 @@ class NewToki : HttpSource(), ConfigurableSource {
     }
 
     override val baseUrl: String
-        get() = preferences.getString(baseUrlPrefKey, defaultBaseUrl) ?: defaultBaseUrl
+        get() = "http://10.0.2.2:8080"
 
     override val lang = "ko"
 
